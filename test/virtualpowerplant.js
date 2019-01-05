76bbb1a2810d73143fb5dda8f0ca787ab1363549
Yan Dev <yan-dev@Yans-MacBook-Pro.local>
2019-01-05 02:11:14 -0600

    add js test details;

--- test/virtualpowerplant.js
+++ test/virtualpowerplant.js
@@ -1,17 +1,107 @@
 const VirtualPowerPlant = artifacts.require("VirtualPowerPlant");
 const BatteryInvestment = artifacts.require("BatteryInvestment");
+const BatteryEnergy = artifacts.require("BatteryEnergy");
 
 contract('VirtualPowerPlant', function(accounts) {
 
-    beforeEach(async () => {
-        VirtualPowerPlantInstance = await VirtualPowerPlant.deployed()
-    })
+    const virtualPowerPlantOwner = accounts[0];
+    const virtualPowerPlantAdmin = accounts[1];
+    const investor1 = accounts[2];
+    const investor2 = accounts[3];
 
-    it("...check VirtualPowerPlant owner address is correct", function() {
-        return VirtualPowerPlantInstance.owner();
-    }).then(function(virtualPowerPlantOwner) {
-        assert.equal(accounts[0], virtualPowerPlantOwner, "Owner address does not match acounts[0] from ganache");
+    let investmentAmount1 = 100;
+    let investmentAmount2 = 150;
+
+    let NewBattery1 = Battery({
+        capacity: 100,
+        currentFilled: 20,
+        cost: 13,
+        serialNumber: "a",
+        priceThreshold: 7,
+        chargeRate: 4
     });
+
+    let NewBattery2 = Battery({
+        capacity: 100,
+        currentFilled: 20,
+        cost: 13,
+        serialNumber: "a",
+        priceThreshold: 7,
+        chargeRate: 4
+    });
+
+
+    // DONE:
+
+  // it("...check VirtualPowerPlant owner address is correct", function() {
+  //   return VirtualPowerPlant.deployed().then(function(instance) {
+  //     virtualPowerPlant = instance;
+  //     return virtualPowerPlant.owner();
+  //     }).then(function(virtualPowerPlantOwner) {
+  //         assert.equal(accounts[0], virtualPowerPlantOwner, "Owner address does not match acounts[0] from ganache");
+  //       });
+  // });
+
+  beforeEach(async () => {
+      virtualPowerPlant = await VirtualPowerPlant.deployed();
+      batteryInvestmentAddress = await (virtualPowerPlant.batteryInvestmentContract());
+      batteryEnergyAddress = await (virtualPowerPlant.batteryEnergyContract());
+      batteryInvestmentContract = await BatteryInvestment.at(batteryInvestmentAddress);
+      batteryEnergyContract = await BatteryEnergy.at(batteryEnergyAddress);
+  })
+
+  // DONE
+
+  // it("...check VirtualPowerPlant owner address is correct", async() => {
+  //     virtualPowerPlantOwnerCheck = await virtualPowerPlant.owner();
+  //     new Promise(() => console.log("VirtualPowerPlant owner address: " + virtualPowerPlantOwnerCheck));
+  //     assert.equal(virtualPowerPlantOwner, virtualPowerPlantOwnerCheck, "Owner address does not match acounts[0] from ganache");
+  // });
+  //
+  // it("...check BatteryInvestment contract is deployed", async() => {
+  //     let batteryInvestmentAddressCheck = await batteryInvestmentContract.address;
+  //     new Promise(() => console.log("BatteryInvestment address: " + batteryInvestmentAddress));
+  //     assert.equal(batteryInvestmentAddress, batteryInvestmentAddressCheck, "BatteryInvestment address doesn't match");
+  // });
+  //
+  // it("...check BatteryEnergy contract is deployed", async() => {
+  //     let batteryEnergyAddressCheck = await batteryEnergyContract.address;
+  //     new Promise(() => console.log("BatteryEnergy address: " + batteryEnergyAddress));
+  //     assert.equal(batteryEnergyAddress, batteryEnergyAddressCheck, "BatteryEnergy address doesn't match");
+  // });
+  //
+  // it("...check BatteryInvestment multiple investments from investor1 and investor2", async() => {
+  //     await batteryInvestmentContract.investMoney({from: investor1, value: investmentAmount1});
+  //     await batteryInvestmentContract.investMoney({from: investor2, value: investmentAmount2});
+  //     let totalInvestment = await batteryInvestmentContract.totalInvestment();
+  //     checkInvestment = investmentAmount1 + investmentAmount2;
+  //     new Promise(() => console.log("totalInvestment: " + totalInvestment));
+  //     assert.equal(totalInvestment, checkInvestment, "Investment amount is incorrect");
+  // });
+  //
+  // it("...check set Admin", async() => {
+  //     await virtualPowerPlant.changeAdmin(virtualPowerPlantAdmin, true, {from: virtualPowerPlantOwner});
+  //     let adminsNum = await virtualPowerPlant.numAdmins();
+  //     adminNumCheck = 1;
+  //     assert.equal(adminsNum, adminNumCheck, "Admin was not added properly");
+  // });
+
+  it("...check add battery to virtualPowerPlant", async() => {
+      await virtualPowerPlant.addBattery(NewBattery1, {from: virtualPowerPlantAdmin});
+      await virtualPowerPlant.addBattery(NewBattery2, {from: virtualPowerPlantOwner});
+      let numBatteries = await (virtualPowerPlant.batteries()).length;
+      assert.equal(numBatteries, 2, "Two batteries added");
+  });
+
+
+
+
+
+
+
+
+
+
 
   // it("...check BatteryInvestment contract is deployed", function() {
   //   return VirtualPowerPlant.deployed().then(function(instance) {
@@ -45,35 +135,39 @@ contract('VirtualPowerPlant', function(accounts) {
   //       });
   // });
 
-  it("...check deployed Battery Investment Contract", async () => {
-      // let batteryInvestment = await BatteryInvestment.deployed();
-      // let virtualPowerPlant = await VirtualPowerPlant.deployed();
-      // const batteryInvestment = await BatteryInvestment.at(virtualPowerPlant.batteryInvestmentAddress());
-
-      // assert(virtualPowerPlant.batteryInvestmentAddress(), virtualPowerPlant.batteryInvestmentContract().address, "addresses match");
-
-      let owner = await virtualPowerPlant.owner();
-      let vplant = await (virtualPowerPlant.batteryInvestmentContract());
-
-      const bi = await BatteryInvestment.at(vplant);
-      let test = await bi.totalInvestment();
-
-      // let instance = await BatteryInvestment.deployed();
-
-      // let next = await vplant.virtualPowerPlantAddress();
-
-      // emit LogNew(vplant.address);
-
-      // assert(owner, next, "addresses match");
-
-      // return virtualPowerPlant.batteryInvestmentAddress();
-      new Promise(() => console.log(test))
-      // new Promise(() => console.log(vplant))
-
-  });
+  // it("...check deployed Battery Investment Contract", async () => {
+  //     // let batteryInvestment = await BatteryInvestment.deployed();
+  //     // let virtualPowerPlant = await VirtualPowerPlant.deployed();
+  //     // const batteryInvestment = await BatteryInvestment.at(virtualPowerPlant.batteryInvestmentAddress());
+  //
+  //     // assert(virtualPowerPlant.batteryInvestmentAddress(), virtualPowerPlant.batteryInvestmentContract().address, "addresses match");
+  //
+  //     let owner = await virtualPowerPlant.owner();
+  //     let vplant = await (virtualPowerPlant.batteryInvestmentContract());
+  //
+  //     const bi = await BatteryInvestment.at(vplant);
+  //     let test = await bi.totalInvestment();
+  //
+  //     // let instance = await BatteryInvestment.deployed();
+  //
+  //     // let next = await vplant.virtualPowerPlantAddress();
+  //
+  //     // emit LogNew(vplant.address);
+  //
+  //     // assert(owner, next, "addresses match");
+  //
+  //     // return virtualPowerPlant.batteryInvestmentAddress();
+  //     new Promise(() => console.log(test))
+  //     // new Promise(() => console.log(vplant))
+  //
+  // });
 
 });
-//
+
+
+
+
+
 // var A = artifacts.require("./A.sol");
 // var B = artifacts.require("./B.sol");
 // contract('A', (accounts) => {
