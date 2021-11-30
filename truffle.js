67af7ae663b631fe702566386683f8ea2f8d659d
yan-man <yan.man45@gmail.com>
2021-11-29 23:18:24 -0600

    update git ignore; truffle config - update with new hdwalletprovider implementation npm;

--- truffle.js
+++ truffle.js
@@ -1,22 +1,23 @@
-const HDWallet = require('truffle-hdwallet-provider');
+const HDWallet = require("@truffle/hdwallet-provider");
 const infuraKey = "1f75a19af87b4563b741fdfc282b7b8f";
 
-const fs = require('fs');
+const fs = require("fs");
 const mnemonic = fs.readFileSync("file.secret").toString().trim();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
-      development: {
-          host: "127.0.0.1",
-          port: 7545,
-          network_id: "*" // Match any network id
-      },
-      rinkeby: {
-        provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
-        network_id: 4,          // Rinkeby's id
-        gas: 6000000,
-      },
-  }
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWallet(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
+      network_id: 4, // Rinkeby's id
+      gas: 6000000,
+    },
+  },
 };
